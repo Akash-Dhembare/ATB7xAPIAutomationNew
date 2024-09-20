6cfe5ffd2f4a5d142b7974802f5afc20249a489e
Akash Dhembare <akash.dhembare777@gmail.com>
2024-09-20 22:41:05 +0530

    cnp

---
+++ src/test/java/com/akash/dhembare2000/ex_15092024/CRUD/POST/NonBDDStylePOST.java
@@ -0,0 +1,67 @@
+package com.akash.dhembare2000.ex_15092024.CRUD.POST;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class NonBDDStylePOST {
+    //  Payload  - String(1%), Hashmap(4%), Classes( 95%)
+
+    //Without @Test
+//    public static void main(String[] args) {
+//        String payload = "{\n" +
+//                "                    \"username\" : \"admin\",\n" +
+//                "                    \"password\" : \"password123\"\n" +
+//                "                }";
+//
+//        // Given - Request Spec
+//        RequestSpecification r = RestAssured.given();
+//        r.baseUri("https://restful-booker.herokuapp.com");
+//        r.basePath("/auth");
+//        r.contentType(ContentType.JSON).log().all();
+//        r.body(payload);
+//
+//        // When -     Response
+//        Response response = r.when().post();
+//
+//
+//        // Then - ValidatableResponse
+//        // Validation
+//        ValidatableResponse validatableResponse = response.then().log().all();
+//        validatableResponse.statusCode(200);
+//    }
+
+    //With @Test
+    @Test
+    public void test_non_bbd_post() {
+
+        //  Payload  - String(1%), Hashmap(4%), Classes( 95%)
+
+        String payload = "{\n" +
+                "                    \"username\" : \"admin\",\n" +
+                "                    \"password\" : \"password123\"\n" +
+                "                }";
+
+        // Given - Request Spec
+        RequestSpecification r = RestAssured.given();
+        r.baseUri("https://restful-booker.herokuapp.com");
+        r.basePath("/auth");
+        r.contentType(ContentType.JSON).log().all();
+        r.body(payload);
+
+        // When -     Response
+        Response response = r.when().post();
+
+
+        // Then - ValidatableResponse
+        // Validation
+        ValidatableResponse validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
+
+
+
+    }
+}
---
+++ src/test/java/com/akash/dhembare2000/ex_15092024/CRUD/PUT/NonBDDPUTStyle.java
@@ -0,0 +1,77 @@
+package com.akash.dhembare2000.ex_15092024.CRUD.PUT;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class NonBDDPUTStyle {
+    RequestSpecification requestSpecification;
+    ValidatableResponse validatableResponse;
+    Response response;
+    // 6118
+    // Token -  Token?
+    // Payload - {
+    //    "firstname" : "Pramod",
+    //    "lastname" : "Brown",
+    //    "totalprice" : 111,
+    //    "depositpaid" : true,
+    //    "bookingdates" : {
+    //        "checkin" : "2018-01-01",
+    //        "checkout" : "2019-01-01"
+    //    },
+    //    "additionalneeds" : "Breakfast"
+    //}
+
+
+    // POST - Auth - token
+    // POST - Booking ID
+    // PUT - token and BookingID
+
+
+//    public void get_token(){
+//
+//    }
+//
+//    public void get_booking_id(){
+//        return bookingid;
+//    }
+
+    @Test
+    public void test_put_positive_tc(){
+        // POST - Auth - token
+
+        String token  = "3dcaa9dcc249381";
+        String bookingid = "2";
+
+        String payloadPUT= "{\n" +
+                "    \"firstname\" : \"Akshu Akash\",\n" +
+                "    \"lastname\" : \"Dhembare\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : false,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2024-01-01\",\n" +
+                "        \"checkout\" : \"2024-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Lunch\"\n" +
+                "}";
+
+
+        requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+        requestSpecification.basePath("/booking/"+bookingid);
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.cookie("token",token);
+        requestSpecification.body(payloadPUT).log().all();
+
+        Response response = requestSpecification.when().put();
+
+
+        // Get Validatable response to perform validation
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
+
+    }
+}
